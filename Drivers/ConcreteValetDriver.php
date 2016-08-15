5454b0e280916822263695325eac2b86117203cd
Dave Rogers <connect@skybluesofa.com>
2016-08-15 18:58:15 -0500

    Added Driver

---
+++ Drivers/ConcreteValetDriver.php
@@ -0,0 +1,55 @@
+<?php
+
+class ConcreteValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        if (file_exists($sitePath.'/concrete/dispatcher.php')) {
+             return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/application/files/'.$uri)) {
+            return $staticFilePath;
+        }
+        if (file_exists($staticFilePath = $sitePath.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath . '/index.php';
+        return $sitePath.'/index.php';
+    }
+}
